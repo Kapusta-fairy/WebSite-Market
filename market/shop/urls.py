e744de8c5ecaa04e35466c07eff5453309ce81b1
Kapusta-fairy <dknenorm63@gmail.com>
2022-08-14 10:38:00 +0300

    add search

--- market/shop/urls.py
+++ market/shop/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import PromoteShop, PoliticShop
+from .views import PromoteShop, PoliticShop, SearchShop
 
 urlpatterns = [
     path('', PromoteShop.as_view(), name='promout'),
     path('politics/', PoliticShop.as_view(), name='politics'),
     path('detail/<slug:slug>/', views.product_detail, name='detail'),
+    path('search/', SearchShop.as_view(), name='search')
 ]
--- market/shop/views.py
+++ market/shop/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView
 from shop.forms import CartAddProductForm
 from shop.models import Products, Politics
+from django.db.models import Q
 
 
 class PromoteShop(ListView):
-    """показывает самые популярные товары"""
     model = Products
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['Title'] = 'Предложения'
         return context
+
+
+class SearchShop(ListView):
+
+    def get_queryset(self):
+        return Products.objects.filter(name=self.request.GET.get('search'))
 
 
 class DetailShop(DetailView):
